bf6960a30db4fc17fe0f55d71dd1eb1b108423ab
Ignacio Paradisi <ignacioparadisi@gmail.com>
2021-04-28 10:48:07 -0500

    Get points from previous view

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -33,36 +33,58 @@ export class MapComponent implements OnInit, OnChanges {
     if (!this.route) { return }
     this.isLoading = true;
     this.showError = false;
-    this.routeService.getRoute(this.route.id).subscribe(route => {
-      this.isLoading = false;
-      console.log(route);
-      this.title = route.name;
-      this.origin = {
-        lat: Number(route.points[0].latitude),
-        lng: Number(route.points[0].longitude),
-      };
-      this.destination = {
-        lat: Number(route.points[route.points.length - 1].latitude),
-        lng: Number(route.points[route.points.length - 1].longitude),
-      };
-      let points = route.points.slice(1, -1);
-      this.waypoints = points.map(point => {
-        return {
-          location: {
-            lat: Number(point.latitude),
-            lng: Number(point.longitude)
-          }
+    this.title = this.route.name;
+    this.origin = {
+      lat: Number(this.route.points[0].latitude),
+      lng: Number(this.route.points[0].longitude),
+    };
+    this.destination = {
+      lat: Number(this.route.points[this.route.points.length - 1].latitude),
+      lng: Number(this.route.points[this.route.points.length - 1].longitude),
+    };
+    let points = this.route.points.slice(1, -1);
+    this.waypoints = points.map(point => {
+      return {
+        location: {
+          lat: Number(point.latitude),
+          lng: Number(point.longitude)
         }
-      });
+      }
+    });
 
-      console.log(this.origin);
-      console.log(this.destination);
-      console.log(this.waypoints)
-    }, error => {
-      this.isLoading = false;
-      this.showError = true;
-      console.log(error);
-    })
+    console.log(this.origin);
+    console.log(this.destination);
+    console.log(this.waypoints)
+    // this.routeService.getRoute(this.route.id).subscribe(route => {
+    //   this.isLoading = false;
+    //   console.log(route);
+    //   this.title = route.name;
+    //   this.origin = {
+    //     lat: Number(route.points[0].latitude),
+    //     lng: Number(route.points[0].longitude),
+    //   };
+    //   this.destination = {
+    //     lat: Number(route.points[route.points.length - 1].latitude),
+    //     lng: Number(route.points[route.points.length - 1].longitude),
+    //   };
+    //   let points = route.points.slice(1, -1);
+    //   this.waypoints = points.map(point => {
+    //     return {
+    //       location: {
+    //         lat: Number(point.latitude),
+    //         lng: Number(point.longitude)
+    //       }
+    //     }
+    //   });
+
+    //   console.log(this.origin);
+    //   console.log(this.destination);
+    //   console.log(this.waypoints)
+    // }, error => {
+    //   this.isLoading = false;
+    //   this.showError = true;
+    //   console.log(error);
+    // })
   }
 
 }
